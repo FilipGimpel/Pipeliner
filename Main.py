3ffe60e514423c61791669e818a4fd7b62983460
Filip <filip.gimpel@gmail.com>
2020-04-18 16:06:26 +0200

    Instroduce Instruction clas, finish simplified variant

--- Main.py
+++ Main.py
@@ -1,56 +1,70 @@
 from typing import List, Dict
 
 
-# filename as script argument
-# maybe do it on the fly?
-def load_instructions_from_file(file_path: str) -> List[List[str]]:
-    instructions: List[List[str]] = []
-    file = open(file_path)
-    for line in file.readlines():
-        a = line.split()
-        instructions.append(a)
-    return instructions
+class Instruction:
+    function: str
+    argument_register_1: str
+    argument_register_2: str
+    result_register: str
+
+    def __init__(self, instruction: List[str]):
+        self.function = instruction[0]
+        self.argument_register_1 = instruction[1]
+        self.argument_register_2 = instruction[2]
+        self.result_register = instruction[3]
+
+    def __str__(self):
+        return "{} {} {} {}".format(
+            self.function,
+            self.argument_register_1,
+            self.argument_register_2,
+            self.result_register
+        )
 
 
-class Pipeliner:
-    register: Dict[str, int] = {}  # better indexing
-    instructions: List[List[str]] = []
+class PipelinerSimplified:
+    cycles_util_available: Dict[str, int] = {}
+    instructions: List[Instruction] = []  # TODO do I need this? I use it in only one function, maybe just pass?
     current_cycle = 0
 
-    def __init__(self, instructions: List[List[str]]):
+    def __init__(self, instructions: List[Instruction]):
         self.instructions = instructions
-        for i in range(0, 10):
-            self.register["R"+str(i)] = 0  # use generator expressions to initialize
+        self.cycles_util_available = {"R{0}".format(i): 0 for i in range(0, 10)}
 
     def process(self):
-        if self.instructions:
-            current_instruction = self.instructions[0]
-            if self.can_execute(current_instruction):
-                self.instructions.pop(0)
-                self.register[current_instruction[3]] = 4
-
-    def process_while(self):
         while self.instructions:
             instruction = self.instructions[0]
             if self.can_execute(instruction):
-                self.register[instruction[3]] = 4
-                print(str(instruction) + " " + str(self.current_cycle + 1))
-                self.instructions.pop(0)  # self.instructions.remove(instruction)
-            if sum(self.register.values()) == 0:
+                self.cycles_util_available[instruction.result_register] = 4
+                print(str(instruction) + " " + str(self.current_cycle + 1))  # TODO Use stringbuffer kinda thing?
+                self.instructions.remove(instruction)
+            if sum(self.cycles_util_available.values()) == 0:
                 break
             self.cycle()
 
-    def can_execute(self, instruction: List[str]) -> bool:  # create instruction class?
-        return (self.register[instruction[1]] <= 0) and (self.register[instruction[2]] <= 1)
+    def can_execute(self, instruction: Instruction) -> bool:
+        return (self.cycles_util_available[instruction.argument_register_1] <= 0) and \
+               (self.cycles_util_available[instruction.argument_register_2] <= 1)
 
     def cycle(self):
         self.current_cycle += 1
         # print(str(self.current_cycle) + "\t" + str(self.register))
-        self.register = {key: (value - 1 if value > 0 else value) for (key, value) in self.register.items()}
+        self.cycles_util_available = \
+            {key: (value - 1 if value > 0 else value) for (key, value) in self.cycles_util_available.items()}
 
 
-list_of_instructions = load_instructions_from_file('input.txt')
-Pipeliner(list_of_instructions).process_while()
+def load_from_file(file_path: str) -> List[Instruction]:
+    instructions: List[Instruction] = []
+    file = open(file_path)
+    for line in file.readlines():
+        a = line.split()
+        instructions.append(Instruction(a))
+    return instructions
+
+
+list_of_instructions = load_from_file('input.txt')
+PipelinerSimplified(list_of_instructions).process()
+
 
 # SIMPLE
 # 1. Jezeli lista instrukcji do wykonania nie jest pusta, wez z niej instrukcje
